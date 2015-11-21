602950e7dc937cde8b899e42cb506dab5b057c6c
lscspirit <lscspirit@hotmail.com>
2015-11-22 03:29:25 +0800

    Update the compile_spec.js and generate_sdk.js to allow spec yml to be split across multiple files.

---
+++ scripts/compile_spec.js
@@ -0,0 +1,56 @@
+var argv = require('minimist')(process.argv.slice(2));
+
+var resolve  = require('json-refs').resolveRefs;
+var traverse = require('traverse');
+var extend  = require('extend');
+var YAML    = require('js-yaml');
+var path    = require('path');
+var fs      = require('fs');
+
+// Argument values
+var spec_path   = path.resolve(argv['_'][0]);
+var output_file = argv['_'].length > 1 ? argv['_'][1] : null;
+
+//
+// Merges all paths and models
+//
+function mergeDirFiles(dir_path) {
+  var files = fs.readdirSync(dir_path);
+  return files.reduce(function(result, filename) {
+    var content = YAML.load(fs.readFileSync(path.join(dir_path, filename)).toString());
+    return extend(result, content);
+  }, {});
+}
+
+var models = mergeDirFiles(path.join(spec_path, 'models'));
+var paths  = mergeDirFiles(path.join(spec_path, 'paths'));
+
+//
+// Load the spec main file
+//
+var spec = YAML.load(fs.readFileSync(path.join(spec_path, 'spec.yml')).toString());
+
+//
+// Pre-process the spec and replace the $spec_ref values
+//
+traverse(spec).forEach(function(value) {
+  if (this.key == '$spec_ref') {
+    if (value == 'models') this.parent.update(models, true);
+    else if (value == 'paths') this.parent.update(paths, true);
+  }
+});
+
+//
+// Resolve external/remote refs
+//
+var options = {
+  location: spec_path,
+  resolveLocalRefs: false,
+  processContent: function (content) {
+    return YAML.load(content);
+  }
+};
+resolve(spec, options).then(function (results) {
+  if (output_file) fs.writeFileSync(output_file, YAML.dump(results.resolved));
+  else console.log(YAML.dump(results.resolved));
+});
---
+++ scripts/generate_sdk.js
@@ -0,0 +1,33 @@
+var argv = require('minimist')(process.argv.slice(2));
+
+var child_process = require('child_process');
+var tmp  = require('tmp');
+var path = require('path');
+
+var swagger_dist   = argv['dist'];
+var custom_codegen = argv['jar'];
+var spec_path   = argv['s'] || argv['spec'];
+var output_path = argv['o'] || argv['output'];
+var debug_mode  = argv['debug'];
+
+function execCmd(cmd) {
+  console.log('===== Executing: ' + cmd + ' ======\n');
+  child_process.execSync(cmd, { stdio: [0,1,2] });
+  console.log('\n');
+}
+
+// swagger classpaths
+var swagger_classes = path.join(swagger_dist, 'target/classes');
+var swagger_lib     = path.join(swagger_dist, 'target/lib') + '/*';
+
+var temp_file = tmp.fileSync();
+
+// Compile the spec into a single temp file
+execCmd(`node ${__dirname}/compile_spec.js ${spec_path} ${temp_file.name}`);
+
+// Generate the sdk from the compiled spec
+var classpaths  = `${swagger_classes}:${swagger_lib}:${custom_codegen}`;
+var config_path = path.join(spec_path, 'config.json');
+var debug_flag  = debug_mode ? `-Ddebug${debug_mode}` : '';
+var cmd = `java ${debug_flag} -cp ${classpaths} io.swagger.codegen.Codegen -l sdkRuby -i ${temp_file.name} -c ${config_path} -o ${output_path}`;
+execCmd(cmd);
